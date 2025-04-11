295058c845c00df3a976a9bdbd3ec669082c898a
Sneha Menat <snehamenat029@gmail.com>
2025-04-11 11:28:29 +0530

    final commity

--- main.py
+++ main.py
@@ -1,49 +1,81 @@
+import os
 import streamlit as st
-from langchain_community.document_loaders import UnstructuredURLLoader
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import FAISS
-import time
 import pickle
+import time
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
+from langchain.embeddings import HuggingFaceEmbeddings
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv() 
+OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY")
 
-st.title("News Research Tool📈")
-st.sidebar.title("News Article URLS")
+st.title("EquiSight: News Research Tool 📈")
+st.sidebar.title("News Article URLs")
+
 urls = []
 for i in range(3):
-  url =  st.sidebar.text_input(f"URL {i+1}")
-  urls.append(url)
+    url = st.sidebar.text_input(f"URL {i+1}")
+    urls.append(url)
 
-process_url_clicked = st.sidebar.button("Process URLS")
-file_path = "faiss_store_openai.pkl"
+process_url_clicked = st.sidebar.button("Process URLs")
+file_path = "vector_index.pkl"
 
-main_placefolder = st.empty()
+main_placeholder = st.empty()
+llm = ChatOpenAI(
+    model_name="deepseek/deepseek-r1-zero:free",  # Specify model
+    openai_api_key=OPEN_AI_API_KEY,
+    base_url="https://openrouter.ai/api/v1"
+)
 
 if process_url_clicked:
+    print(urls)
+    #load data
     loader = UnstructuredURLLoader(urls=urls)
-    main_placefolder.text("Data Loading...Started...")
+    main_placeholder.text("Data Loading...Started...✅✅✅")
     data = loader.load()
-    print(len(data))
-    #SPLIT DATA
+    print("Data")
+    # # split data
     text_splitter = RecursiveCharacterTextSplitter(
-       separators = ['\n\n', '\n', '.', ','],
-       chunk_size = 1000
+        separators=['\n\n', '\n', '.', ','],
+        chunk_size=1000
     )
-
-    main_placefolder.text("Data Loading...Completed...")
-    main_placefolder.text("Text Splitter...Started...")
-
+    main_placeholder.text("Text Splitter...Started...✅✅✅")
     docs = text_splitter.split_documents(data)
-
-
-    #Create embeddings
-    model_name = "sentence-transformers/all-mpnet-base-v2"  
+    print("Docs",len(docs))
+    # create embeddings and save it to FAISS index
+    model_name = "sentence-transformers/all-mpnet-base-v2"
     embeddings = HuggingFaceEmbeddings(model_name=model_name)
-    vectorindex_openai = FAISS.from_documents(docs, embeddings)
-
-    main_placefolder.text("Embedding vector started Building....")
+    vectorstore_openai = FAISS.from_documents(docs, embeddings)
+    main_placeholder.text("Embedding Vector Started Building...✅✅✅")
     time.sleep(2)
 
+    # # Save the FAISS index to a pickle file
     with open(file_path, "wb") as f:
-       pickle.dump(vectorindex_openai, f)
+        pickle.dump(vectorstore_openai, f)
+
+query = main_placeholder.text_input("Question: ")
+if query:
+    if os.path.exists(file_path):
+        with open(file_path, "rb") as f:
+            vectorstore = pickle.load(f)
+            chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
+            result = chain({"question": query}, return_only_outputs=True)
+            # result will be a dictionary of this format --> {"answer": "", "sources": [] }
+            st.header("Answer")
+            st.write(result["answer"])
+
+            # Display sources, if available
+            sources = result.get("sources", "")
+            if sources:
+                st.subheader("Sources:")
+                sources_list = sources.split("\n")  # Split the sources by newline
+                for source in sources_list:
+                    st.write(source)
+
+
+
